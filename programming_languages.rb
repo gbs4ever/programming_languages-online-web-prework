800d7212ff519fa1d926fb8eae3cffd2430bd955
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-01 05:44:36 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,17 +1,16 @@
 require "pry"
 def reformat_languages(languages)
-h= {}
+  h= {}
   languages.each do |k,lang_hash|
-  lang_hash.each do |lang,style|
-h[lang]=style
-binding.pry
+    lang_hash.each do |lang,style|
+      h[lang]=style
+      binding.pry
+#add_to_hash=k
+    #h=lang_hash
+    #hash=v.keys
+    
 
-    add_to_hash=k
-    h=lang_hash
-    hash=v.keys
-    binding.pry
-
-     h[]={:style =>[ ]}
+     #h[]={:style =>[ ]}
 #  h[language][:style]     <<add_to_hash
 
 end
